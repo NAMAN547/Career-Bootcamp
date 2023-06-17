294fe77e58bca8a94cdc4805c649967a223c72de
NAMAN TYAGI <namantyagi33@gmail.com>
2023-06-17 23:39:06 +0530

    Lecture36

---
+++ Lecture36/ecommerce-v3/models/User.js
@@ -0,0 +1,20 @@
+
+const mongoose = require("mongoose");
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
+const userSchema = new mongoose.Schema({
+
+    // username : String, ---> This is automatically added by passport-local-mongoose, no need to add ourselves
+    // password : String,  ---> This is automatically added by passport-local-mongoose
+
+    email : String
+})
+
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema)
+
+
+module.exports = User;
